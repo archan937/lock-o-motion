a47307ece8d6b4040236a1a09df55da42a4aa9b6
Paul Engel <paul.engel@holder.nl>
2013-02-09 23:34:35 +0100

    Corrected Lotion.resolve

--- lib/motion/lotion.rb
+++ lib/motion/lotion.rb
@@ -19,7 +19,7 @@ private
     if path.match /^\//
       path
     else
-      path = path.gsub(/\.rb.*$/, ".rb")
+      path = path.gsub(/\.rb.*$/, "") + ".rb"
       LOAD_PATHS.each do |load_path|
         if File.exists?(absolute_path = "#{load_path}/#{path}")
           return absolute_path
