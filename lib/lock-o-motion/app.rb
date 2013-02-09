ac675e35c4dd44febc9a3438fdea2d6723071f36
Paul Engel <paul.engel@holder.nl>
2013-02-09 23:34:56 +0100

    Simplified Lotion::App.require

--- lib/lock-o-motion/app.rb
+++ lib/lock-o-motion/app.rb
@@ -7,11 +7,7 @@ module LockOMotion
       if call == File.expand_path("../../lock-o-motion.rb", __FILE__)
         call = File.expand_path(GEM_LOTION)
       end
-      begin
-        Kernel.require path, call
-      rescue LoadError
-        Kernel.require File.expand_path(path), call
-      end
+      Kernel.require path, call
     end
 
   end
