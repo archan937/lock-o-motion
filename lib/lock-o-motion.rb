e51215ac1ed960d9762c56afdece327f9faf4c29
Paul Engel <paul.engel@holder.nl>
2013-02-10 05:08:05 +0100

    Moved LockOMotion methods to LockOMotion::App and improved them

--- lib/lock-o-motion.rb
+++ lib/lock-o-motion.rb
@@ -1,115 +1,25 @@
 require "colorize"
-
 require "motion/gem_ext"
+
 require "lock-o-motion/app"
 require "lock-o-motion/version"
 
 module LockOMotion
   extend self
 
-  APP_FILE    = File.expand_path("../lock-o-motion/app.rb", __FILE__)
-  GEM_LOTION  = ".lotion.rb"
-  USER_LOTION = "lotion.rb"
-
-  def setup
+  def setup(&block)
     Motion::Project::App.setup do |app|
-      files_dependencies = catch_files_dependencies do
-        Bundler.require :lotion
-        LockOMotion::App.require "colorize"
-        yield LockOMotion::App if block_given?
-      end
-
-      bundler_dependencies = files_dependencies.delete("BUNDLER") || []
-      app.files = (bundler_dependencies + app.files + default_files + (files_dependencies.keys + files_dependencies.values).flatten.sort).uniq
-      app.files_dependencies files_dependencies
-
-      write_lotion app
+      LockOMotion::App.setup app, &block
     end
   end
 
-  def skipped?(path)
+  def latest_load_paths
+    @latest_load_paths ||= Dir["{#{::Gem.paths.path.join(",")}}" + "/gems/*/lib"]
+  end
+
+  def skip?(path)
     !!%w(openssl pry).detect{|x| path.match %r{\b#{x}\b}}.tap do |file|
       puts "   Warning Skipped '#{file}' requirement".yellow if file
-    end
-  end
-
-private
-
-  def catch_files_dependencies(&block)
-    hook = proc do
-      def require_with_catch(path, call = nil)
-        return if LockOMotion.skipped?(path)
-        hash = Thread.current[:catched_files_dependencies]
-
-        if call || caller[0].match(/^(.*\.rb)/)
-          call ||= $1
-          call = "BUNDLER" if call.match(/\bbundler\b/)
-          file = "#{path.gsub(/\.rb$/, "")}.rb"
-
-          if call != APP_FILE && (load_path = $:.detect{|x| File.exists?("#{x}/#{file}")})
-            (hash[call] ||= []) << "#{load_path}/#{file}"
-          end
-        end
-
-        require_without_catch path
-      end
-      alias :require_without_catch :require
-      alias :require :require_with_catch
-    end
-
-    unhook = proc do
-      alias :require :require_without_catch
-      undef :require_with_catch
-      undef :require_without_catch
-    end
-
-    Thread.current[:catched_files_dependencies] = {}
-    Kernel.instance_eval &hook
-    Object.class_eval &hook
-
-    block.call
-
-    Kernel.instance_eval &unhook
-    Object.class_eval &unhook
-    Thread.current[:catched_files_dependencies].tap do |dependencies|
-      Thread.current[:catched_files_dependencies] = nil
-    end
-  end
-
-  def default_files
-    [
-      File.expand_path("../motion/core_ext.rb", __FILE__),
-      File.expand_path("../motion/lotion.rb", __FILE__),
-     (File.expand_path(USER_LOTION) if File.exists?(USER_LOTION)),
-      File.expand_path(GEM_LOTION)
-    ].compact
-  end
-
-  def write_lotion(app)
-    FileUtils.rm GEM_LOTION if File.exists?(GEM_LOTION)
-    File.open(GEM_LOTION, "w") do |file|
-      file << <<-RUBY_CODE.gsub("        ", "")
-        module Lotion
-          LOAD_PATHS = #{pretty_inspect $:, 2}
-          REQUIRED = #{pretty_inspect $", 2}
-          FILES = #{pretty_inspect app.files, 2}
-          DEPENDENCIES = #{pretty_inspect app.dependencies, 2}
-        end
-      RUBY_CODE
-    end
-  end
-
-  def pretty_inspect(object, indent = 0)
-    if object.is_a?(Array)
-      entries = object.collect{|x| "  #{pretty_inspect x, indent + 2}"}
-      entries.each_with_index{|x, i| entries[i] = "#{x}," if i < entries.size - 1}
-      ["[", entries, "]"].flatten.join "\n" + (" " * indent)
-    elsif object.is_a?(Hash)
-      entries = object.collect{|k, v| "  #{k.inspect} => #{pretty_inspect v, indent + 2}"}
-      entries.each_with_index{|x, i| entries[i] = "#{x}," if i < entries.size - 1}
-      ["{", entries, "}"].flatten.join "\n" + (" " * indent)
-    else
-      object.inspect
     end
   end
 
--- lib/lock-o-motion/app.rb
+++ lib/lock-o-motion/app.rb
@@ -1,13 +1,141 @@
 module LockOMotion
-  module App
-    extend self
+  class App
+
+    GEM_LOTION  = ".lotion.rb"
+    USER_LOTION =  "lotion.rb"
+
+    def self.setup(app, &block)
+      new(app).send :setup, &block
+    end
+
+    def initialize(app)
+      @app = app
+    end
 
     def require(path)
-      call = caller[0].match(/^(.*\.rb)/) && $1
-      if call == File.expand_path("../../lock-o-motion.rb", __FILE__)
-        call = "BUNDLER"
+      Kernel.require path
+    end
+
+    def ignore_require(path)
+      @ignored_requires << path
+    end
+
+    def dependency(call, path)
+      call = "BUNDLER" if call.match(/\bbundler\b/)
+      call = "CUSTOM"  if call == __FILE__
+      path = "#{path.gsub(/\.rb$/, "")}.rb"
+
+      ($: + LockOMotion.latest_load_paths).each do |load_path|
+        if File.exists?(absolute_path = "#{load_path}/#{path}")
+          (@dependencies[call] ||= []) << absolute_path
+          $:.unshift load_path unless $:.include?(load_path)
+          return
+        end
       end
-      Kernel.require path, call
+
+      puts "   Warning Could not resolve dependency \"#{path}\"".yellow
+    end
+
+  private
+
+    def setup(&block)
+      @files = []
+      @dependencies = {}
+      @ignored_requires = []
+
+      Thread.current[:lotion_app] = self
+      Kernel.instance_eval &hook
+      Object.class_eval &hook
+
+      Bundler.require :lotion
+      require "colorize"
+      yield self if block_given?
+
+      Kernel.instance_eval &unhook
+      Object.class_eval &unhook
+      Thread.current[:lotion_app] = nil
+
+      bundler = @dependencies.delete("BUNDLER") || []
+      custom  = @dependencies.delete("CUSTOM")  || []
+      (bundler + custom).each do |file|
+        (@dependencies[file] ||= []).concat default_files
+      end
+
+      @files = (default_files + bundler.sort + (@dependencies.keys + @dependencies.values).flatten.sort + custom.sort + @app.files).uniq
+      @files << File.expand_path(USER_LOTION) if File.exists?(USER_LOTION)
+
+      @app.files = @files
+      @app.files_dependencies @dependencies
+      write_lotion
+    end
+
+    def hook
+      @hook ||= proc do
+        def require_with_catch(path)
+          return if LockOMotion.skip?(path)
+          if caller[0].match(/^(.*\.rb)/)
+            Thread.current[:lotion_app].dependency $1, path
+          end
+          begin
+            require_without_catch path
+          rescue LoadError
+            if load_path = LockOMotion.latest_load_paths.detect{|x| File.exists? "#{x}/#{path}"}
+              $:.unshift load_path
+              require_without_catch path
+            end
+          end
+        end
+        alias :require_without_catch :require
+        alias :require :require_with_catch
+      end
+    end
+
+    def unhook
+      @unhook ||= proc do
+        alias :require :require_without_catch
+        undef :require_with_catch
+        undef :require_without_catch
+      end
+    end
+
+    def default_files
+      @default_files ||= [
+        File.expand_path("../../motion/core_ext.rb", __FILE__),
+        File.expand_path("../../motion/lotion.rb", __FILE__),
+        File.expand_path(GEM_LOTION)
+      ]
+    end
+
+    def write_lotion
+      FileUtils.rm GEM_LOTION if File.exists?(GEM_LOTION)
+      File.open(GEM_LOTION, "w") do |file|
+        file << <<-RUBY_CODE.gsub("          ", "")
+          module Lotion
+            GEM_PATHS = #{pretty_inspect LockOMotion.latest_load_paths, 2}
+            LOAD_PATHS = #{pretty_inspect $:, 2}
+            REQUIRED = #{pretty_inspect $", 2}
+            FILES = #{pretty_inspect @files, 2}
+            DEPENDENCIES = #{pretty_inspect @dependencies, 2}
+            IGNORED_REQUIRES = #{pretty_inspect @ignored_requires, 2}
+          end
+        RUBY_CODE
+      end
+    end
+
+    def pretty_inspect(object, indent = 0)
+      if object.is_a?(Array)
+        entries = object.collect{|x| "  #{pretty_inspect x, indent + 2}"}
+        return "[]" if entries.empty?
+        entries.each_with_index{|x, i| entries[i] = "#{x}," if i < entries.size - 1}
+        ["[", entries, "]"].flatten.join "\n" + (" " * indent)
+      elsif object.is_a?(Hash)
+        entries = object.collect{|k, v| "  #{k.inspect} => #{pretty_inspect v, indent + 2}"}
+        return "{}" if entries.empty?
+        entries.each_with_index{|x, i| entries[i] = "#{x}," if i < entries.size - 1}
+        ["{", entries, "}"].flatten.join "\n" + (" " * indent)
+      else
+        object.inspect
+      end
     end
 
   end
