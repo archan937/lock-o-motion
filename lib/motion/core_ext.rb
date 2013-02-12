c2bf5ee82709baecc64620dca2ac231df1c26be9
Paul Engel <paul.engel@holder.nl>
2013-02-12 01:42:28 +0100

    Allowing `class_eval`, `module_eval` and `instance_eval` at runtime when passing a block

--- lib/motion/core_ext.rb
+++ lib/motion/core_ext.rb
@@ -5,11 +5,21 @@ module Kernel
 end
 
 class Module
+  alias :original_class_eval :class_eval
   def class_eval(*args, &block)
-    Lotion.warn name, :class_eval, caller
+    if block_given?
+      original_class_eval &block
+    else
+      Lotion.warn name, :class_eval, caller
+    end
   end
+  alias :original_module_eval :module_eval
   def module_eval(*args, &block)
-    Lotion.warn name, :module_eval, caller
+    if block_given?
+      original_module_eval &block
+    else
+      Lotion.warn name, :module_eval, caller
+    end
   end
 end
 
@@ -28,7 +38,12 @@ class Object
   def require(*args, &block)
     Lotion.require args.first, caller
   end
+  alias :original_instance_eval :instance_eval
   def instance_eval(*args, &block)
-    Lotion.warn self.class.name, :instance_eval, caller
+    if block_given?
+      instance_eval &block
+    else
+      Lotion.warn self.class.name, :instance_eval, caller
+    end
   end
 end
