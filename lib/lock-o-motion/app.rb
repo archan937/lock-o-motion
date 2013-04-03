48591b8c235a194f1317a54fa85a1db1d53a55ae
Paul Engel <paul.engel@holder.nl>
2013-04-03 23:27:13 +0200

    Extracted .lotion.rb writer methods to App::Writer module

--- lib/lock-o-motion/app.rb
+++ lib/lock-o-motion/app.rb
@@ -1,5 +1,8 @@
+require "lock-o-motion/app/writer"
+
 module LockOMotion
   class App
+    include Writer
 
     def self.setup(app, &block)
       new(app).send :setup, &block
@@ -133,38 +136,7 @@ module LockOMotion
 
     end
 
-    def write_lotion
-      FileUtils.rm GEM_LOTION if File.exists?(GEM_LOTION)
-      File.open(GEM_LOTION, "w") do |file|
-        file << <<-RUBY_CODE.gsub("          ", "")
-          module Lotion
-            FILES = #{pretty_inspect @files, 2}
-            DEPENDENCIES = #{pretty_inspect @dependencies, 2}
-            IGNORED_REQUIRES = #{pretty_inspect @ignored_requires, 2}
-            MOCKS_DIRS = #{pretty_inspect LockOMotion.mocks_dirs, 2}
-            LOAD_PATHS = #{pretty_inspect $:, 2}
-            GEM_PATHS = #{pretty_inspect LockOMotion.gem_paths, 2}
-            REQUIRED = #{pretty_inspect $", 2}
-          end
-        RUBY_CODE
-      end
-    end
 
-    def pretty_inspect(object, indent = 0)
-      if object.is_a?(Array)
-        entries = object.collect{|x| "  #{pretty_inspect x, indent + 2}"}
-        return "[]" if entries.empty?
-        entries.each_with_index{|x, i| entries[i] = "#{x}," if i < entries.size - 1}
-        ["[", entries, "]"].flatten.join "\n" + (" " * indent)
-      elsif object.is_a?(Hash)
-        entries = object.collect{|k, v| "  #{k.inspect} => #{pretty_inspect v, indent + 2}"}
-        return "{}" if entries.empty?
-        entries.each_with_index{|x, i| entries[i] = "#{x}," if i < entries.size - 1}
-        ["{", entries, "}"].flatten.join "\n" + (" " * indent)
-      else
-        object.inspect
-      end
-    end
 
   end
 end
